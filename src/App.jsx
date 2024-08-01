9cbd1812ca5aff7f259f4f39849401beab5a3a2b
Byeongjae Kang <50051883+ByeongjaeKang@users.noreply.github.com>
2024-08-01 23:44:14 +0900

    feat: tape rolls out from the ruler.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import { useEffect, useState, useRef } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { CameraControls, useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 import './App.css';
 
 function App() {
   const { nodes, materials } = useGLTF("/measuring_tape.glb");
-  const [length, setLength] = useState(0);
+  let [length, setLength] = useState(0);
+  const tapeShape = new THREE.Shape();
+  tapeShape.moveTo(0, 0);
+  tapeShape.lineTo(.3, 0);
+  tapeShape.lineTo(.3, .01);
+  tapeShape.lineTo(0, .01);
+  tapeShape.lineTo(0, 0);
 
-  setInterval(() => {
-    setLength(length + 1);
-  }, 1500);
+  const tapeExtrudeConf = {
+    depth: length,
+    bevelEnabled: false,
+  };
 
   return (
     <>
+      <main>
+        <h3>Wheeler</h3>
+        <p>Use your ordinary mouse as a tape ruler.</p>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="50"
+            step={0.1}
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+          />
+          <span>Length: {length}</span>
+        </div>
+      </main>
       <Canvas
         style={{
           width: '100vw',
@@ -25,9 +47,9 @@ function App() {
           minPolarAngle={0}
           maxPolarAngle={Math.PI / 0.5}
         />
-        <pointLight
-          position={[3, 3, 3]}
-          intensity={32}
+        <ambientLight
+          position={[2, 3, 3]}
+          intensity={16}
           color="#FFF"
         />
 
@@ -36,7 +58,7 @@ function App() {
           geometry={nodes.measuring_tape_01.geometry}
           material={materials.measuring_tape_01}
           position={[2, 0, 0]}
-          rotation={[Math.PI / 8, 11.8, 0]}
+          rotation={[Math.PI / 64, 12.55, 0]}
           scale={16}
         >
           <meshBasicMaterial
@@ -44,11 +66,13 @@ function App() {
           />
         </mesh>
 
-        {/* test */}
-        <mesh
-          position={[5, 0, 0]}
-        >
-          <boxGeometry attach="geometry" args={[1, 0.005, length]} /> 
+        {/* test (sliced tape) */}
+        <mesh position={[1.84, 0.155, 0.65]}>
+          {/* <boxGeometry attach="geometry" args={[1, 0.005, length]} /> */}
+          <extrudeGeometry
+            name="slicedTape"
+            args={[tapeShape, tapeExtrudeConf]}
+          />
           <meshStandardMaterial attach="material" color="yellow" />
         </mesh>
 
